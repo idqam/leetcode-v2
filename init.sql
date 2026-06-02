b2b21bb98ccf351375790160246acb99fab83c36
Owen <85533286+idqam@users.noreply.github.com>
2026-06-01 19:24:34 -0500

    deploy

---
+++ init.sql
@@ -0,0 +1,7 @@
+-- Database is already initialized by env vars
+-- Just verify the leetcode database exists and is owned by tracker
+-- ALTER DATABASE leetcode OWNER TO tracker;
+SELECT 1 as "initialized";
+
+
+
